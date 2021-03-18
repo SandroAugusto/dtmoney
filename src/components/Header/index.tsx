30eefd4cfdd4f81fbab04449bf12bf18a3e33f50
SandroAugusto <sandro.oaugusto@gmail.com>
2021-03-17 22:46:23 -0300

    chore commit inicial

---
+++ src/components/Header/index.tsx
@@ -0,0 +1,20 @@
+import logoImg from '../../assets/logo.svg';
+import { Container, Content } from './styles';
+
+interface Headerprops {
+    onOpenNewTransactionModal: () => void;
+}
+
+export function Header({ onOpenNewTransactionModal }: Headerprops) {
+
+    return (
+        <Container>
+            <Content>
+            <img src={logoImg} alt="dt money"/>
+            <button type="button" onClick={onOpenNewTransactionModal}>
+                Nova Transação
+            </button>
+            </Content>
+        </Container>
+    )
+}
